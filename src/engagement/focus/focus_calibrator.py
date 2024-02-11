2aa1e6aa0a13e4fcee9400729f8a067ce546047a
johnpl-io <john.nycjets@gmail.com>
2024-02-11 15:32:44 -0500

    initial non vectorized focus complete

--- src/engagement/focus/focus_calibrator.py
+++ src/engagement/focus/focus_calibrator.py
@@ -33,37 +33,44 @@ class Focus_Calibrator:
         """function used to determine focus on a linear scale.
         determines fraction of distance from center of focus to outside focus
         on a linear scale."""
-
+        distance = lambda x, y : math.sqrt((x)**2 + (y)**2)
         #find distance from center
         x_distance = (gaze_angle_x-self.gaze_angle_xcenter) 
         y_distance = (gaze_angle_y-self.gaze_angle_ycenter)
-        distance_from_center_to_gaze = math.sqrt((x_distance)**2 + (y_distance)**2)
+        distance_from_center_to_gaze = distance(x_distance, y_distance)
         
 
         radians = math.atan2(y_distance, x_distance)
         slope = y_distance/x_distance
-        breakpoint()
-        b = self.gaze_angle_ycenter-slope*self.gaze_xcenter
+        
+        b = self.gaze_angle_ycenter-slope*self.gaze_angle_xcenter
 
         radians = math.atan2(y_distance, x_distance)
-
+    
         if(radians <= math.pi/4 and radians > -math.pi/4):
             #right wall
             y = slope*self.gaze_angle_xmax+b
-            distance_from_center = 
+            distance_from_center = distance(self.gaze_angle_xmax-self.gaze_angle_xcenter, y - self.gaze_angle_ycenter)
+    
         elif(radians > math.pi/4 and radians <= (3*math.pi/4)):
             #upper wall
             x = (self.gaze_angle_ymax - b)/slope
+            distance_from_center = distance(x-self.gaze_angle_xcenter, self.gaze_angle_ymax - self.gaze_angle_ycenter)
+
         elif(radians > (3*math.pi/4) and radians >= -(3*math.pi/4)):
             #left wall
             y = slope*self.gaze_angle_xmin+b
+            distance_from_center = distance(self.gaze_angle_xmin - self.gaze_angle_xcenter, y - self.gaze_angle_ycenter)
         else:
+            #bottom wall
             x = (self.gaze_angle_ymax - b)/slope
-        
+            distance_from_center = distance(x - self.gaze_angle_xcenter, self.gaze_angle_ymin - self.gaze_angle_ycenter)
 
-        return 2
+        eye_gaze_focus = 1 - (distance_from_center_to_gaze/distance_from_center)      
+        
+        return eye_gaze_focus
     
 
     #test
         
-Focus_Calibrator('~/processed/demo.csv')(0.06, 0.2)
+print(Focus_Calibrator('~/processed/demo.csv')(-0.2, 0.06))
---
+++ src/engagement/focus/test.py
@@ -0,0 +1,6 @@
+import numpy as np 
+
+
+a = np.array([0, 1, 2])
+b = a[a > 1] 
+print(b)
